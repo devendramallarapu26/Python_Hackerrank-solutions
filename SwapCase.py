b6ed9c378a46dd5d5c0f0cc38d5f21600fd883ed
Devendra <devendramallarapu8@gmail.com>
2024-11-05 21:35:59 +0530

    Create SwapCase.py

---
+++ SwapCase.py
@@ -0,0 +1,13 @@
+def swap_case(s):
+    s = s.swapcase()
+    return s
+
+if __name__ == '__main__':
+    s = input()
+    result = swap_case(s)
+    print(result)
+
+# Input:
+# HackerRank.com presents "Pythonist 
+# output:
+# hACKERrANK.COM PRESENTS "pYTHONIST 2".
